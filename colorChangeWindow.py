2429d7e0158f7d82cfa8709004536d2897fbe72e
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-03-09 08:48:07 -0500

    Updated from Brython Server: 3/9/2018 8:48:06 AM

--- colorChangeWindow.py
+++ colorChangeWindow.py
@@ -1,1 +1,17 @@
+#Shaylee McBride
+#9Mar2018
+#colorChangeWindow.py
 
+from ggame import *
+from random import randint
+
+outline = LineStyle(0,Color(0x000000,1)
+
+def mouseClick(event):
+    color = '0x'
+    for i in range(6):
+        rand = randint(0,9)
+        rand = str(rand)
+        color = color+rand
+    
+    background = RectangleAsset(600,1200,outline,color)
