583221d329b32f0bb7352a05e2c0ff266c77a231
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-03-09 08:37:05 -0500

    Updated from Brython Server: 3/9/2018 8:37:04 AM

--- countdownn.py
+++ countdownn.py
@@ -1,1 +1,11 @@
+#Shaylee McBride
+#9Mar2018
+#countdownn.py - when the hell are my crocs getting here
 
+def countdownn(n):
+    while n >= 1:
+        print(n)
+        n -= 1
+    print('BOOM')
+
+countdownn(4)
