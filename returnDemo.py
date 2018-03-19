c4ea12620406e254a3695c63fcfbb7528847ee80
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-03-19 08:18:03 -0400

    Updated from Brython Server: 3/19/2018 8:18:03 AM

--- returnDemo.py
+++ returnDemo.py
@@ -1,1 +1,13 @@
+#Shaylee McBride
+#19March2018
+#returnDemo.py - how to use return
 
+from random import randint
+
+def randEven(low,high):
+    n=randint(low,high)
+    while n%2 != 0:
+        n = randint(low,high)
+    return(n)
+
+print("your even numbers are",randEven(0,100),randEven(0,100),randEven(1,100))
