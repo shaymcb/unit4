40643e66bbc26a58b4e07ad9029aec1d8194eae5
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-03-19 08:06:18 -0400

    Update functionDemo.py

--- functionDemo.py
+++ functionDemo.py
@@ -17,10 +17,13 @@ def bigger(a,b):
 def slope(x1, y1, x2, y2):
     print((y2-y1)/(x2-x1))
 
+print('the max of 3 and 4 is',max(3,4))
+print('the max of 3 and 4 is',bigger(3,4)) #need to use return, not print
+
+"""
 slope(1,-1,2,2)
 slope(True,True,False,False) #sketchy
 
-"""
 bigger(4,3)
 bigger('hi','hoe you thought')
 bigger(False,True)
