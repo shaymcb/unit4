78fc1b29b1cddf9526689be341224d709c45828f
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-03-19 08:48:43 -0400

    Updated from Brython Server: 3/19/2018 8:48:43 AM

--- distance.py
+++ distance.py
@@ -1,1 +1,11 @@
+#Shaylee McBride
+#19Mar2018
+#distance.py - distance formula
 
+from math import sqrt 
+
+def distance(x1,y1,x2,y2):
+    d = sqrt((x1-x2)**2+(y1-y2)**2)
+    return d
+
+print(distance(3,4,-5,2))
