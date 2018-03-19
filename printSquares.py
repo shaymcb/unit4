64cd6df2d358d07cdc1d472987727ab2f3ef7155
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-03-19 08:44:41 -0400

    Updated from Brython Server: 3/19/2018 8:44:41 AM

--- printSquares.py
+++ printSquares.py
@@ -1,1 +1,11 @@
+#Shaylee McBride
+#19March2018
+#printSquares.py
 
+def printSquares(rows,columns):
+    for i in range(columns):
+        print("+--"*rows+'+')
+        print("|  "*(rows+1))
+    print("+--"*rows+'+')
+
+printSquares(3,5)
