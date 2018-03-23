7d6d01709b8b873a711df6ef260b7223c847547d
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-03-23 08:20:32 -0400

    Updated from Brython Server: 3/23/2018 8:20:31 AM

--- monkeyBanana.py
+++ monkeyBanana.py
@@ -10,41 +10,42 @@ ROWS = 28  #all caps for constants
 COLS = 50
 CELL_SIZE = 20
 
+#functions
 def moveRight(event):
     if monkey.x < ((COLS-1)*CELL_SIZE):
         monkey.x += CELL_SIZE
         if monkey.x == banana.x and monkey.y == banana.y:
             moveBanana()
-            data['score'] += 10
-            print(data['score'])
+            updateScore()
     
 def moveLeft(event):
     if monkey.x > 0:
         monkey.x -= CELL_SIZE
         if monkey.x == banana.x and monkey.y == banana.y:
             moveBanana()
-            data['score'] += 10
-            print(data['score'])
+            updateScore()
         
 def moveUp(event):
     if monkey.y > 0:
         monkey.y -= CELL_SIZE
         if monkey.x == banana.x and monkey.y == banana.y:
             moveBanana()
-            data['score'] += 10
-            print(data['score'])
+            updateScore()
         
 def moveDown(event):
     if monkey.y < ((ROWS-1)*CELL_SIZE):
         monkey.y += CELL_SIZE
         if monkey.x == banana.x and monkey.y == banana.y:
             moveBanana()
-            data['score'] += 10
-            print(data['score'])
+            updateScore()
 
 def moveBanana():
     banana.x = randint(0,COLS-1)*CELL_SIZE
     banana.y = randint(0,ROWS-1)*CELL_SIZE
+
+def updateScore():
+    data['score'] += 10
+    print(data['score'])
 
 if __name__ == '__main__':
     
@@ -60,10 +61,12 @@ if __name__ == '__main__':
     jungleBox = RectangleAsset(CELL_SIZE*COLS,CELL_SIZE*ROWS,LineStyle(2,green),green)
     monkeyBox = RectangleAsset(CELL_SIZE,CELL_SIZE,LineStyle(2,brown),brown)
     bananaBox = RectangleAsset(CELL_SIZE,CELL_SIZE,LineStyle(2,green),yellow)
+    scoreBox = TextAsset('Score = 0')
     
     Sprite(jungleBox)
     monkey = Sprite(monkeyBox)
     banana = Sprite(bananaBox,(CELL_SIZE*COLS/2,CELL_SIZE*ROWS/2))
+    Sprite(scoreBox,(0,ROWS*CELL_SIZE)
     
     App().listenKeyEvent('keydown','right arrow',moveRight)
     App().listenKeyEvent('keydown','left arrow',moveLeft)
