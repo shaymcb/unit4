aafdda970667a6122d3e22db9ab1beb9bb53e541
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-03-26 08:41:18 -0400

    Updated from Brython Server: 3/26/2018 8:41:17 AM

--- stringUnion.py
+++ stringUnion.py
@@ -1,1 +1,12 @@
+#Shaylee McBride
+#26Mar2018
+#stringUnion.py - prints all the letters in at least one word
 
+def stringUnion(word1,word2):
+    combined = ''
+    for ch in word1+word2:
+        if ch not in combined:
+            combined = combined + ch.lower()
+    return combined
+    
+print(stringUnion('Pennsylvania','Mississippi'))
