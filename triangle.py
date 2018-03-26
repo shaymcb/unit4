2ed5f780f6a526d30502a5c85c1322da0ac8dfd5
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-03-26 09:18:32 -0400

    Updated from Brython Server: 3/26/2018 9:18:31 AM

--- triangle.py
+++ triangle.py
@@ -1,1 +1,31 @@
+#Shaylee McBride
+#26Mar2018
+#triangle.py - area of triangle!!
 
+from math import sqrt 
+
+def distance(x1,y1,x2,y2):
+    d = sqrt((x1-x2)**2+(y1-y2)**2)
+    return d
+
+def fuckingHeron(a,b,c):
+    s = 1/2 * (a+b+c)
+    area = sqrt(
+    
+
+if __name__ == '__main__':  
+    #inputs
+    x1 = float(input('x1 = '))
+    y1 = float(input('y1 = '))
+    x2 = float(input('x2 = '))
+    y2 = float(input('y2 = '))
+    x3 = float(input('x3 = '))
+    y3 = float(input('y3 = '))
+    
+    #distances:
+    a = distance(x1,y1,x2,y2)
+    b = distance(x2,y2,x3,y3)
+    c = distance(x3,y3,x1,y1)
+    
+    print(fuckingHeron(a,b,c))
+    
