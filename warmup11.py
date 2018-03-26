fd390c2a242e8ad214f22575ddc227d4d0c38821
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-03-26 08:15:41 -0400

    Updated from Brython Server: 3/26/2018 8:15:41 AM

--- warmup11.py
+++ warmup11.py
@@ -1,1 +1,15 @@
+#Shaylee McBride
+#26Mar2018
+#warmup11.py - primes
 
+def prime(num):
+    i = 2
+    divisors = 0
+    while i < num:
+        if num%i == 0:
+            num = num/i
+            divisors += 1
+            i += 1
+    print(divisors == 0)
+
+prime(5)
