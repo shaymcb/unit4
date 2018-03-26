c96615bce61c99fad7e055a3d6e7f60c10f80b49
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-03-26 08:45:15 -0400

    Updated from Brython Server: 3/26/2018 8:45:15 AM

--- stringIntercept.py
+++ stringIntercept.py
@@ -1,1 +1,14 @@
+#Shaylee McBride
+#26Mar2018
+#stringIntercept.py - where do the strings overlap?
 
+def stringIntercept(word1,word2):
+    word1 = word1.lower()
+    word2 = word2.lower()
+    combined = ''
+    for ch in word1:
+        if ch in word2 and ch not in combined:
+            combined = combined + ch
+    return combined
+
+print(stringIntercept('Mississippi','Pennsylvania'))
