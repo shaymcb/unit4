e1caab8118ae62405cd35fb840849c186611c9d8
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-03-29 08:20:01 -0400

    Updated from Brython Server: 3/29/2018 8:20:00 AM

--- warmup12.py
+++ warmup12.py
@@ -1,1 +1,15 @@
+#Shaylee McBride
+#29Mar2018
+#warmup12.py - GCF
 
+def GCF(num1,num2):
+    i = min(num1,num2)
+    while True:
+        if num1%i==0 and num2%i==0:
+            return i
+        i -= 1
+        
+
+print(GCF(12,15))
+print(GCF(5,9))
+print(GCF(16,64))
