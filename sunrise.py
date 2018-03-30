0e504a422af396192b20770fc2ea093f137b0d81
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-03-30 08:49:08 -0400

    Updated from Brython Server: 3/30/2018 8:49:07 AM

--- sunrise.py
+++ sunrise.py
@@ -11,21 +11,24 @@ SCREENHEIGHT = 700
 def rise():
     sun.y -= 0.5
     
+def redden():
+    data['skycolor'] = int(data['skyColor']) + 20000
+    if data['skycolor'] < 100000:
+        data['skycolor'] = '0'+str(data(skyColor))
+    skyBox = RectangleAsset(SCREENWIDTH,SCREENHEIGHT,LineStyle(0,Color(0x000000,1)),Color('0x'+str(data['skycolor']),1))
+    Sprite(skyBox)
 
 if __name__ == '__main__':
     data = {}
-    data['skyred'] = '00'
-    data['skygreen'] = '00'
-    data['skyblue'] = '00'
-    data['skycolor']='0x'+data['skyred']+data['skygreen']+data['skyblue']
+    data['skycolor']='000000'
     
     groundBox = RectangleAsset(SCREENWIDTH, 200, LineStyle(0,Color(0x000000,1)),Color(0x006600,1))
-    skyBox = RectangleAsset(SCREENWIDTH,SCREENHEIGHT,LineStyle(0,Color(0x000000,1)),Color(data['skycolor'],1))
+    skyBox = RectangleAsset(SCREENWIDTH,SCREENHEIGHT,LineStyle(0,Color(0x000000,1)),Color('0x'+data['skycolor'],1))
     sunShape = CircleAsset(100,LineStyle(0,Color(0x000000,1)),Color(0xFFFF00,1))
     
     sky = Sprite(skyBox)
     sun = Sprite(sunShape,(SCREENWIDTH/2-100,SCREENHEIGHT-150))
     Sprite(groundBox, (0,SCREENHEIGHT-200))
     
-    App().run(rise)
+    App().run(redden)
     
