c67a0c244cb00179ce930b4e6b1be8623f950137
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-04-02 08:16:38 -0400

    Updated from Brython Server: 4/2/2018 8:16:38 AM

--- quiz4.py
+++ quiz4.py
@@ -1,1 +1,26 @@
+#Shaylee McBride
+#2Apr2018
+#quiz4.py
 
+def count(num):
+    for i in range(1,num+1):
+        print(i)
+
+def excitedPrint(string):
+    print(string.upper()+'!!!')
+
+def firstLetter(string):
+    for ch in string:
+        return(ch)
+
+def repeats(first,second,third):
+    if first == second or first == third or second == third:
+        return True
+    else:
+        return False
+
+count(4)
+excitedPrint('death is inevitable')
+print(firstLetter('forgetful'))
+print(repeats(3,4,4))
+print(repeats('four',4,'For'))
